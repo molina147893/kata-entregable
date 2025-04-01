bfe8f81fa3c349852bb9b8921e267246e1ed8be2
molina147893 <cuestionado.agr@gmail.com>
2025-04-01 13:14:13 +0200

    [refactor] - Cambiar ifs a operador ternario

--- src/Kata.php
+++ src/Kata.php
@@ -12,12 +12,7 @@ class Kata
 
         $product =  strtolower($parts[1]);
 
-        if(isset($parts[2])){
-            $quantity = $parts[2];
-        }
-        if(!isset($parts[2])){
-            $quantity = "1";
-        }
+        $quantity = isset($parts[2]) ? $parts[2] : "1";
 
         return $product . " x" . $quantity;
 
